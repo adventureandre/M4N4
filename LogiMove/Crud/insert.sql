6ade4038396d1c275a480660e9a0b3c37590a2c8
Andre luiz <adventureandre@hotmail.com>
2024-05-31 13:46:51 -0300

    Missao Pratica

--- LogiMove/Crud/insert.sql
+++ LogiMove/Crud/insert.sql
@@ -1,2 +1,14 @@
-INSERT INTO Drivers (Nome, CNH, Endereco, Contato) VALUES ('Andre Luiz', '1122334455', 'rua prof julita', 'admin@adventure.dev.br');
+INSERT INTO Drivers (Nome, CNH, Endereco, Contato) VALUES ('Andre Luiz', '1122334455', 'rua prof julita','admin@adventure.dev.br')
+
+INSERT INTO Drivers (Nome, CNH, Endereco, Contato) VALUES ('João Silva', '1234567890', 'Rua A, 123', 'joao@example.com')
+
+INSERT INTO Drivers (Nome, CNH, Endereco, Contato) VALUES ('Maria Oliveira', '0987654321', 'Rua B, 456','maria@example.com')
+
+INSERT INTO Clients (Nome, Empresa, Endereco, Contato) VALUES ('Milena Camila', 'ALFA Estetica', 'Av. Principal, 53','admin@funilariaepintura.com.br')
+
+INSERT INTO Clients (Nome, Empresa, Endereco, Contato) VALUES ('Sivaldo', 'SP Transportes', 'Rua Prof julita, 43','contato@sp.com')
+
+INSERT INTO Orders (ClientID, DriverID, DetalhesPedido, DataEntrega, Status) VALUES (1, 1, 'pedido em preparaçã','2024-06-01', 'Pendente')
+
+INSERT INTO Orders (ClientID, DriverID, DetalhesPedido, DataEntrega, Status) VALUES (2, 2, 'Entrega do pedido','2024-06-02', 'Em andamento')
 
